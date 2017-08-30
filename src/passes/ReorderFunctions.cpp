e904392db53546b75af9e8253566aba02fec8a50
Alon Zakai (kripken) <alonzakai@gmail.com>
2017-08-30 15:29:27 -0700

    parallelize ReorderFunctions

--- src/passes/ReorderFunctions.cpp
+++ src/passes/ReorderFunctions.cpp
@@ -15,12 +15,19 @@
  */
 
 //
-// Sorts functions by their static use count. This helps reduce the size of wasm
-// binaries because fewer bytes are needed to encode references to frequently
-// used functions.
+// Sorts functions to reduce the size and improve compressibility of
+// the output binary. This considers two factors:
+//  * Functions with many uses should get lower indexes, so each call
+//    to them is smaller.
+//  * Similar functions should be close together, so that compression
+//    is more effective. For example, a C++ template might generate
+//    two almost-identical functions that differ in just one byte,
+//    and if they are close they might almost be compressed to helf
+//    their combined size. TODO
 //
 
 
+#include <atomic>
 #include <memory>
 
 #include <wasm.h>
@@ -28,34 +35,62 @@
 
 namespace wasm {
 
-struct ReorderFunctions : public WalkerPass<PostWalker<ReorderFunctions>> {
-  std::map<Name, uint32_t> counts;
+typedef std::unordered_map<Name, std::atomic<Index>> FunctionUseMap;
 
-  void visitModule(Module *module) {
-    if (module->start.is()) {
-      counts[module->start]++;
-    }
-    for (auto& curr : module->exports) {
-      counts[curr->value]++;
-    }
-    for (auto& segment : module->table.segments) {
-      for (auto& curr : segment.data) {
-        counts[curr]++;
-      }
-    }
-    std::sort(module->functions.begin(), module->functions.end(), [this](
-      const std::unique_ptr<Function>& a,
-      const std::unique_ptr<Function>& b) -> bool {
-      if (this->counts[a->name] == this->counts[b->name]) {
-        return strcmp(a->name.str, b->name.str) > 0;
-      }
-      return this->counts[a->name] > this->counts[b->name];
-    });
-    counts.clear();
+struct FunctionUseCounter : public WalkerPass<PostWalker<FunctionUseCounter>> {
+  bool isFunctionParallel() override { return true; }
+
+  FunctionUseCounter(FunctionUseMap* uses) : uses(uses) {}
+
+  FunctionUseCounter* create() override {
+    return new FunctionUseCounter(uses);
   }
 
   void visitCall(Call *curr) {
-    counts[curr->target]++;
+    (*uses)[curr->target]++;
+  }
+
+private:
+  FunctionUseMap* uses;
+};
+
+
+struct ReorderFunctions : public Pass {
+  void run(PassRunner* runner, Module* module) override {
+    // Prepare to count uses in functions. We fill with 0 so
+    // that we do not modify the vector in parallel
+    FunctionUseMap uses;
+    for (auto& func : module->functions) {
+      uses[func->name] = 0;
+    }
+    // Find uses in function bodies
+    {
+      PassRunner runner(module);
+      runner.setIsNested(true);
+      runner.add<FunctionUseCounter>(&uses);
+      runner.run();
+    }
+    // Find global uses
+    if (module->start.is()) {
+      uses[module->start]++;
+    }
+    for (auto& curr : module->exports) {
+      uses[curr->value]++;
+    }
+    for (auto& segment : module->table.segments) {
+      for (auto& curr : segment.data) {
+        uses[curr]++;
+      }
+    }
+    // sort by number of uses
+    std::sort(module->functions.begin(), module->functions.end(), [&uses](
+      const std::unique_ptr<Function>& a,
+      const std::unique_ptr<Function>& b) -> bool {
+      if (uses[a->name] == uses[b->name]) {
+        return strcmp(a->name.str, b->name.str) > 0;
+      }
+      return uses[a->name] > uses[b->name];
+    });
   }
 };
 
